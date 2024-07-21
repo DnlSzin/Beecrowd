a0544dedbfa24adb0a34b159c33a3daae36fad3b
Daniel <69518683+DnlSzin@users.noreply.github.com>
2024-07-21 20:03:23 -0300

    Create 1010-Cálculo Simples.c

---
+++ 1010-Cálculo Simples.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int a1,a2,b1,b2;
+float a,b;
+ scanf("%d %d %f",&a1,&a2,&a);
+ scanf("%d %d %f",&b1,&b2,&b);
+ printf("VALOR A PAGAR: R$ %.2f\n",((a2*a)+(b2*b)));
+    return 0;
+}
